4fda890cb9374b06c73c19529e3894ccbd050b7e
ferza17 <feryreza85@gmail.com>
2021-01-19 19:51:31 +0700

    add strategy, template method

---
+++ Template_Method/Funtional_Template_Method/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func PlayGame(
+	start, takeTurn func(),
+	haveWinner func() bool,
+	winningPlayer func() int,
+) {
+	start()
+	for ; !haveWinner(); {
+		takeTurn()
+	}
+	fmt.Printf("Player %d wins. \n", winningPlayer())
+}
+
+func main() {
+	turn, maxTurn, currentPlayer := 1, 10, 0
+
+	start := func() {
+		fmt.Println("Starting game of chess")
+	}
+
+	takeTurn := func() {
+		turn++
+		fmt.Printf("Turn %d taken by player %d\n", turn, currentPlayer)
+		currentPlayer = (currentPlayer + 1) % 2
+	}
+
+	haveWinner := func() bool {
+		return turn == maxTurn
+	}
+
+	winningPlayer := func() int {
+		return currentPlayer
+	}
+
+	PlayGame(start, takeTurn, haveWinner, winningPlayer)
+
+}
